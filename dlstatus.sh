bef402288976378c86896b0f7add6c303acfde8a
Alessandro Tedeschi <tedeschia1047@my.sduhsd.net>
2020-12-18 17:04:37 +0000

    My first commit

---
+++ dlstatus.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+   then
+	FILES=( dl01 dl02 dl03 dl04 dl05 dl06 dl07 dl08 dl09 dl10 dl11 )
+   else
+	FILES=( "$@" )
+
+fi
+
+for file in "${FILES[@]}"
+do
+   RESULT=`ping -c 1 ${file}`
+   if [ $? == 0 ]
+	then
+	   echo "${file}: Alive"
+	else
+	   echo "${file}: Dead"
+	fi
+done
+
+
+
+
